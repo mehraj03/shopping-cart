53386fcc10bd3455c6210eeea0a8983ae9877684
mehraj03 <mehrajshafat@gmail.com>
2026-04-07 09:02:46 -0400

    Add ShoppingList with 10 items

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("1. Apples");
+        System.out.println("2. Bread");
+        System.out.println("3. Milk");
+        System.out.println("4. Eggs");
+        System.out.println("5. Butter");
+        System.out.println("6. Chicken");
+        System.out.println("7. Rice");
+        System.out.println("8. Pasta");
+        System.out.println("9. Tomatoes");
+        System.out.println("10. Cheese");
+    }
+}
